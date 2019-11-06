399e3d7fd82158618847a05973707257053a95c8
Mitch Cox <mitch@enox.co.za>
2019-11-06 21:20:21 +0200

    wip

--- v2/flukymcq/src/App.vue
+++ v2/flukymcq/src/App.vue
@@ -1,70 +1,63 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-      dense
-      elevation="2"
-    >
-    
+    <v-app-bar app color="primary" dark dense elevation="2">
       <div>
         <v-icon large>mdi-blur-linear</v-icon>
       </div>
       <v-spacer></v-spacer>
-      <div class="title font-weight-bold">
-        Fluky MCQ Marking System 
-      </div>
+      <div class="title font-weight-bold" style="font-family:'Poiret One'!important;">Fluky MCQ Marking System</div>
 
       <v-spacer></v-spacer>
 
-      <v-btn color="orange darken-1" 
+      <v-btn
+        color="orange darken-1"
         href="https://www.buymeacoffee.com/UhCeEp6"
-        target="_blank" rounded>
+        target="_blank"
+        rounded
+      >
         <v-img src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg" contain height="25"></v-img>
       </v-btn>
     </v-app-bar>
 
     <v-content>
-      <FlukyV2/>
+      <FlukyV2 />
     </v-content>
 
-    <v-footer 
-      app
-      dark
-      >
-      <div class="body-2">
-        <v-icon small>mdi-settings</v-icon>
-        Created by Mitchell A. Cox :: Please double check Fluky's output as the creator(s) cannot be held liable for errors.
-      </div>
-  
-        <v-spacer/>
+    <v-footer app dark>
+      <div
+        class="body-2"
+      >Created by Mitchell A. Cox :: Please double check Fluky's output as the creator(s) cannot be held liable for errors.</div>
 
-        <v-btn class="mx-2 white--text" icon small
-          href="https://github.com/ManCaveMade/FlukyMCQ"
-          target="_blank">
-          <v-icon>mdi-github-circle</v-icon>
-        </v-btn>
-        <v-btn class="mx-2 white--text" icon small
-          href="mailto:mitchell.cox@wits.ac.za">
-          <v-icon>mdi-email</v-icon>
-        </v-btn>
-</v-footer>
+      <v-spacer />
+      <div class="body-2">Version 2.0</div>
+      <v-btn
+        class="mx-2 white--text"
+        icon
+        small
+        href="https://github.com/ManCaveMade/FlukyMCQ"
+        target="_blank"
+      >
+        <v-icon>mdi-github-circle</v-icon>
+      </v-btn>
+      <v-btn class="mx-2 white--text" icon small href="mailto:mitchell.cox@wits.ac.za">
+        <v-icon>mdi-email</v-icon>
+      </v-btn>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import FlukyV2 from './components/FlukyV2';
+import FlukyV2 from "./components/FlukyV2";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    FlukyV2,
+    FlukyV2
   },
 
   data: () => ({
     //
-  }),
+  })
 };
 </script>
--- v2/flukymcq/src/components/FlukyV2.vue
+++ v2/flukymcq/src/components/FlukyV2.vue
@@ -1,10 +1,10 @@
 <template>
   <v-container fluid>
-    <v-stepper v-model="e6" vertical>
-      <v-stepper-step :complete="e6 > 1" step="1" editable>About Fluky</v-stepper-step>
+    <v-stepper v-model="step" vertical>
+      <v-stepper-step :complete="step > 1" step="1" editable>About Fluky</v-stepper-step>
 
       <v-stepper-content step="1" class="body-2">
-        <strong>Welcome to Fluky v2.0!</strong>
+        <h4>Welcome to Fluky v2.0!</h4>
         <p>
           Everything you need to do is on this one page.
           Nothing is stored by Fluky on the server and all the processing is done by your browser using JavaScript code - this avoids any possible security risks.
@@ -19,42 +19,101 @@
             target="_blank"
           >github.com/ManCaveMade/FlukyMCQ/wiki</a>
         </p>
-      
-        <v-card class="d-inline-flex" outlined tile>
+
+        <h4>Notes and "Pro Tips"</h4>
+        <ul>
+          <li>Since the original Fluky, several enhancements have been made, but existing functionality has been maintained.</li>
+          <li>Please log issues, feature requests, etc. on the GitHub page. Please remember that Fluky is open source, has no warranty and is developed in my spare time - mostly so that I can spend more time on my research... ;)</li>
+          <li>Feel free to add to the wiki linked above, but if you prefer, please email me your "Pro Tips" and I'll add them somewhere.</li>
+        </ul>
+        <p />
+        <h4>Acknowledgements</h4>
+        <p>
+          Thank you to everyone who has given me feedback and ideas for Fluky. In particular, I'd like to acknowledge Doug Clerk from Physics for our long discussions on good MCQ testing. Also, thank
+          <i>you</i> for using Fluky - it motivates me to continue development!
+        </p>
+
+        <div class="pt-4 pb-2">
+          <v-btn outlined color="primary" @click="step = 2">Continue</v-btn>
+        </div>
+      </v-stepper-content>
+
+      <v-stepper-step :complete="step > 2" step="2" editable>Setup Mark Model</v-stepper-step>
+
+      <v-stepper-content step="2">
+        <h4>Summary</h4>
+        <p>
+          Create your mark model here by first specifying the number of questions in your test, and the total marks required for 100%.
+          Next, fill in the marks that should be allocated to each choice in the "Mark Matrix", as well as the "Maximum Mark" value for each question. Negative and part marks are allowed.
+          In essence, Fluky will perform a weighted sum of this matrix with the A to E choices on the MCQ card, subject to some configurable rules and options.
+        </p>
+        <h4>Explanation of Rules and Options</h4>
+        <ul>
+          <li>
+            The mark for a question will be made zero if the total choices made is greater than the specified "Max. Choices".
+            This alleviates the need for naive negative marking used to prevent excess marks by "blanketing" the MCQ card.
+          </li>
+          <li>The mark for a question will be capped to the maximum specified for that question. If the "Maximum Mark" is left as zero, this rule will not be applied.</li>
+          <li>
+            In the case of negative marking, the mark for a question will be floored to the minimum specified for that question. If left as the default value of zero, a negative question mark will not be carried into the test total, preventing student panic. This is useful for questions which require multiple "correct" selections, where some
+            <i>very</i> wrong options are made negative.
+          </li>
+          <li>"Knowledge Areas" are an optional feature where Fluky will save you time by summing specified groups of questions.</li>
+        </ul>
+        <p />
+        <h4>Pro Tips</h4>
+        <ul>
+          <li>
+            The Wits scanner is
+            <strong>very</strong> sensitive and will detect even erased cells on a card. Do not allow students to erase. Rather get them to fill out a fresh MCQ card.
+          </li>
+          <li>A trade-off to consider is making each question allow two answers, to compensate for extreme scanner sensitivity. Some students may get lucky - evaluate your options.</li>
+          <li>To prevent card-wastage, tell the students to do working and record their selections in their answer books, and only hand out the MCQ cards towards the end of the test (or upon request).</li>
+        </ul>
+        <p />
+
+        <v-card class="mx-auto" outlined elevation="1">
+          <v-card-title>Test Setup</v-card-title>
           <v-card-text>
-          <p><strong>Pro Tip:</strong> Look out for boxes like this one - they contain "extra" instructions and pro tips...</p>
-          <strong>Changes to v2.0:</strong> Since the original Fluky, several enhancements have been made, but existing functionality has been maintained. Please log issues, feature requests, etc. on the GitHub page. Please remember that Fluky is open source, has no warranty and is developed in my spare time. :) 
+            <v-row>
+              <v-col cols="12" sm="6" md="6">
+                <v-text-field
+                  v-model="testSetup.numQuestions"
+                  type="number"
+                  label="Number of Questions"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12" sm="6" md="6">
+                <v-text-field
+                  v-model="testSetup.totalMarks"
+                  type="number"
+                  label="Total Marks (100%)"
+                ></v-text-field>
+              </v-col>
+            </v-row>
           </v-card-text>
         </v-card>
 
         <div class="pt-4 pb-2">
-          <v-btn outlined color="primary" @click="e6 = 2">Continue</v-btn>
+          <v-btn outlined color="primary" @click="step = 3">Continue</v-btn>
+          <v-btn class="ml-2" text @click="step = 1">Back</v-btn>
         </div>
       </v-stepper-content>
 
-      <v-stepper-step :complete="e6 > 2" step="2" editable>Setup Mark Model</v-stepper-step>
-
-      <v-stepper-content step="2">
-        
-
-        <div class="pt-4 pb-2">
-          <v-btn outlined color="primary" @click="e6 = 2">Continue</v-btn>
-          <v-btn class="ml-2" text @click="e6 = 1">Back</v-btn>
-        </div>
-      </v-stepper-content>
-
-      <v-stepper-step :complete="e6 > 3" step="3" editable>Select an ad format and name ad unit</v-stepper-step>
+      <v-stepper-step :complete="step > 3" step="3" editable>Upload Card Scan</v-stepper-step>
 
       <v-stepper-content step="3">
         <v-card color="grey lighten-1" class="mb-12" height="200px"></v-card>
-        <v-btn color="primary" @click="e6 = 4">Continue</v-btn>
+        <v-btn color="primary" @click="step = 4">Continue</v-btn>
         <v-btn text>Cancel</v-btn>
       </v-stepper-content>
 
-      <v-stepper-step step="4" editable>View setup instructions</v-stepper-step>
+      <v-stepper-step step="4" editable>View Statistics and Download Results</v-stepper-step>
       <v-stepper-content step="4">
         <v-card color="grey lighten-1" class="mb-12" height="200px"></v-card>
-        <v-btn color="primary" @click="e6 = 1">Continue</v-btn>
+        <v-btn color="primary" @click="step = 1">Continue</v-btn>
         <v-btn text>Cancel</v-btn>
       </v-stepper-content>
     </v-stepper>
@@ -67,10 +126,9 @@ export default {
     source: String
   },
   data: () => ({
-    e6: 1
+    step: 1,
+    testSetup: { totalMarks: 0, numQuestions: 1 }
   }),
-  created() {
-    this.$vuetify.theme.dark = false;
-  }
+  created() {}
 };
 </script>
